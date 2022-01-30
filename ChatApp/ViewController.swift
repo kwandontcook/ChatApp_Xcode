67ecbff242d8d65317eb7733710bc1cbbc58356a
Kwok Chung Kwan <kwandontcook@gmail.com>
2022-01-30 16:10:09 +1030

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Mv Mobile on 30/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
